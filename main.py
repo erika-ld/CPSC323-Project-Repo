a16f863748439567214eb341482ae48b4ec160c7
Erika Dickson <ErikaLD@csu.fullerton.edu>
2024-02-28 01:23:00 -0800

    Update main() function to open & read the first test case .txt file.
    Implemented first part of the pseudocode from the slides to read from the .txt files.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def dfsm_int(input):
     table = [[0 for i in range(alphabets)] for j in range(states)]
     print(table)
     return 1
-dfsm_int(12)
+
 
 
 def dfsm_real(input):
@@ -13,20 +13,34 @@ def dfsm_real(input):
     print(table)
     return 1
 
-dfsm_real(12)
+
 
 def dfsm_id(input):
     states, alphabets = (7,4)
     table = [[0 for i in range(alphabets)] for j in range(states)]
     print(table)
+
     return 1
 
-dfsm_id(12)
 
 def lexer():
     return 1
 
 def main():
+    
+    #Open file_one (test_case_one.txt) for reading purposes.
+    file_one = open('test_case_one.txt' , 'r')
+
+    #While loop to read characters from file_one.
+    while True:
+        char = file_one.read(1)          
+        if not char: 
+            break
+        print(char)
+
+    #Close file_one.
+    file_one.close()
+
     print(lexer())
 
 if __name__ == "__main__":
