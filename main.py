e69c20bd6deb2869089107ff61b0159dd0e91309
Erika Dickson <ErikaLD@csu.fullerton.edu>
2024-02-28 15:48:04 -0800

    Updated some things

--- main.py
+++ main.py
@@ -20,26 +20,26 @@ def dfsm_id(input):
     table = [[0 for i in range(alphabets)] for j in range(states)]
     print(table)
 
-    return 1
-
 
 def lexer(input):
     #Call the FSMs from this function.
 
-    while True:
-        #endif else function integer boolean real if return print scan while endwhile true false
-        if input == ('endif' or 'else' or 'function' or 'integer' or 
-        'boolean' or 'real' or 'if' or 'return' or 'print' or 'scan' or 'while' or 'endwhile'):
-            #process keyword
-            return 1
-        elif input[0].isalpha():
-            #Call DFSM Id
-            print(input[0])
-            dfsm_id_output = dfsm_id()
-        else:
-            break
-    
-    return 1
+    #Keywords: endif else function integer boolean real if return print scan while endwhile true false
+    if (input == 'endif' or 'else' or 'function' or 'integer' or 'true' or 'false'
+    'boolean' or 'real' or 'if' or 'return' or 'print' or 'scan' or 'while' or 'endwhile'):
+        #process keyword
+        ex = "keyword found"
+        print(ex)
+
+    #elif input[0].isalpha():
+        #Call DFSM Id
+        #print(input[0])
+        #dfsm_id_output = dfsm_id(input)
+        #print(dfsm_id_output)
+    #else:
+        #break
+
+    return 0
 
 
 def main():
@@ -51,12 +51,9 @@ def main():
     print(token_list)
     for i in token_list:
         lexer(i)
-    #Need help implementing while loop to reach end of file.
 
     #Close file_one.
     file_one.close()
-
-    #print(lexer())
 
 if __name__ == "__main__":
     main()
