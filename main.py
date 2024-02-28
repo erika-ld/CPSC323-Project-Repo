ecaead4d2c82e6050114abf4064ba76d79b4def6
Erika Dickson <ErikaLD@csu.fullerton.edu>
2024-02-28 00:34:56 -0800

    Update main.py w/ lists for transition table
    Updated the transition table to include the state reached by an illegal character & the alphabet input of an illegal character

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 
-def fsm_int(input):
-    states, alphabets = (2, 2)
-    table = [[0 for i in range(states)] for j in range(alphabets)]
+def dfsm_int(input):
+    states, alphabets = (3,2)
+    table = [[0 for i in range(alphabets)] for j in range(states)]
+    print(table)
+    return 1
+dfsm_int(12)
+
+
+def dfsm_real(input):
+    states, alphabets = (5,3)
+    table = [[0 for i in range(alphabets)] for j in range(states)]
     print(table)
     return 1
 
+dfsm_real(12)
 
-def fsm_real():
-    states, alphabets = (2, 2)
-    table = [[0 for i in range(states)] for j in range(alphabets)]
+def dfsm_id(input):
+    states, alphabets = (7,4)
+    table = [[0 for i in range(alphabets)] for j in range(states)]
     print(table)
     return 1
 
-def fsm_id():
-    states, alphabets = (2, 2)
-    table = [[0 for i in range(states)] for j in range(alphabets)]
-    print(table)
-    return 1
+dfsm_id(12)
 
 def lexer():
     return 1
