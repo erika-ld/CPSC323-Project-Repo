836b737d9d6bcd95c9bb75a4d1c8aea605489560
Erika Dickson <ErikaLD@csu.fullerton.edu>
2024-04-07 03:30:18 -0700

    Added the framework for 35 functions
    Just a general framework; not yet filled in, just saving some time to type it out. Still missing 1-2 functions, unsure as to what they are

---
+++ syntax_analyzer.py
@@ -0,0 +1,153 @@
+import lexical
+
+#36-37 functions for each non-terminal 
+
+print_switch = True
+
+def Rat24S():
+    if print_switch:
+        print("[Rule]")
+
+def Optional_Function_Definitions():
+    if print_switch:
+        print("[Rule]")
+    
+def Optional_Declaration_List():
+    if print_switch:
+        print("[Rule]")
+
+def Statement_List():
+    if print_switch:
+        print("[Rule]")
+
+def Function_Definition():
+    if print_switch:
+        print("[Rule]")
+
+def Empty():
+    if print_switch:
+        print("[Rule]")
+
+def Function():
+    if print_switch:
+        print("[Rule]")
+
+def Identifier():
+    if print_switch:
+        print("[Rule]")
+
+def Optional_Parameter_List():
+    if print_switch:
+        print("[Rule]")
+
+def Body():
+    if print_switch:
+        print("[Rule]")
+
+def Parameter_List():
+    if print_switch:
+        print("[Rule]")
+
+def Parameter():
+    if print_switch:
+        print("[Rule]")
+
+def IDs():
+    if print_switch:
+        print("[Rule]")
+
+def Qualifier():
+    if print_switch:
+        print("[Rule]")
+
+def Declaration_List():
+    if print_switch:
+        print("[Rule]")
+
+def Declaration():
+    if print_switch:
+        print("[Rule]")
+
+def Statement():
+    if print_switch:
+        print("[Rule]")
+
+def Compound():
+    if print_switch:
+        print("[Rule]")
+
+def Assign():
+    if print_switch:
+        print("[Rule]")
+
+def If():
+    if print_switch:
+        print("[Rule]")
+
+def If_Prime():
+    if print_switch:
+        print("[Rule]")
+
+def Return():
+    if print_switch:
+        print("[Rule]")
+
+def Print():
+    if print_switch:
+        print("[Rule]")
+
+def Scan():
+    if print_switch:
+        print("[Rule]")
+
+def While():
+    if print_switch:
+        print("[Rule]")
+
+def Expression():
+    if print_switch:
+        print("[Rule]")
+
+def Expression_Prime():
+    if print_switch:
+        print("[Rule]")
+
+def Condition():
+    if print_switch:
+        print("[Rule]")
+
+def Relop():
+    if print_switch:
+        print("[Rule]")
+
+def Term():
+    if print_switch:
+        print("[Rule]")
+
+def Term_Prime():
+    if print_switch:
+        print("[Rule]")
+
+def Factor():
+    if print_switch:
+        print("[Rule]")
+
+def Primary():
+    if print_switch:
+        print("[Rule]")
+
+def Integer():
+    if print_switch:
+        print("[Rule]")
+
+def Real():
+    if print_switch:
+        print("[Rule]")
+
+
+
+def main():
+    print("")
+
+if __name__ == "__main__":
+    main()
