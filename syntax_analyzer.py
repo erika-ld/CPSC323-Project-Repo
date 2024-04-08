28fca6b264e72cecddc13210aa57221ad4b5bcc4
Erika Dickson <ErikaLD@csu.fullerton.edu>
2024-04-07 20:02:40 -0700

    Updated R25 with Janda's code

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -180,10 +180,19 @@ def Relop():
 
 #R25. <Expression> ::= <Expression> + <Term> | <Expression> - <Term> | <Term>
 def Expression():
-    if print_switch:
-        print("<Expression> ::= <Expression> + <Term> | <Expression> - <Term> | <Term>")
+  global token_index 
+  if print_switch:
+    print("<Expression> ::= <Expression> + <Term> | <Expression> - <Term> | <Term>")
+    print("Revised: <Expression> ::= <Term> <Expression_Prime>")  
+  # Parse the first term
+  Term()
 
-
+   # Check if the next token is either '+' or '-'
+  if lexical.get_lexeme(token_index) == '+' or lexical.get_lexeme(token_index) == '-':
+        # Move to the next token
+        token_index += 1
+        # Parse the next term
+        Term()
 
 def Expression_Prime():
     if print_switch:
